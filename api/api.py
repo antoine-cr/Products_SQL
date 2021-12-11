f4b4f85faf5c458f2792c2e98f77a83cd2b8a99f
antoine-cr <92589163+antoine-cr@users.noreply.github.com>
2021-12-11 11:55:43 +0100

    api creation

---
+++ api/api.py
@@ -0,0 +1,69 @@
+from typing import List
+from typing import List, Optional
+from pydantic import BaseModel
+from fastapi import FastAPI, Depends, Header, HTTPException, status
+
+from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, text, inspect
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from sqlalchemy.orm import Session
+
+Base = declarative_base()
+
+class Products(Base):
+    __tablename__ = "products"
+    uniq_id = Column(String, primary_key=True, index=True)
+    product_name = Column(String, unique=True, index=True)
+    amazon_category_and_sub_category = Column(String)
+    manufacturer = Column(Integer)
+    price = Column(String)
+    number_available_in_stock = Column(String)
+    number_of_reviews = Column(Integer)
+    number_of_answered_questions= Column(Integer)
+    average_rating = Column(String)
+
+#Création de la BDD
+engine = create_engine('sqlite:///transfers.db', echo=True)
+conn = engine.connect()
+
+# Test
+# stmt = text ( "SELECT * from transfers inner join players on players.id=transfers.player_id limit 5;" )
+# result = conn.execute(stmt)
+# print(result.fetchall())
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+api = FastAPI()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+security = HTTPBasic()
+
+
+def get_product(db: Session, product_uniq_id: int):
+    return db.query(Products).filter(Products.uniq_id == product_uniq_id).first()
+
+def get_products(db: Session, skip: int = 0, limit: int = 50):
+    return db.query(Products).offset(skip).limit(limit).all()
+
+@api.get("/products/")
+def search_products(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    players = get_product(db, skip=skip, limit=limit)
+    return players
+
+@api.get("/products/{uniq_id}")
+def search_product(uniq_id: int, db: Session = Depends(get_db)):
+    product = get_products(db, product_uniq_id=uniq_id)
+    if product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return product
