d49743bf26978d2c3af692008a060ad5268d702c
antoine-cr <92589163+antoine-cr@users.noreply.github.com>
2021-12-11 16:38:20 +0100

    api works

--- api/api.py
+++ api/api.py
@@ -11,12 +11,14 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from sqlalchemy.orm import Session
+from sqlalchemy.sql.sqltypes import ARRAY, FLOAT, VARCHAR, String, Integer
+from sqlalchemy.sql.type_api import STRINGTYPE
 
 Base = declarative_base()
 
 class Products(Base):
-    __tablename__ = "products"
-
+    __tablename__ = "product"
+    id = Column(Integer, index=True)
     uniq_id = Column(String, primary_key=True, index=True)
     product_name = Column(String, unique=True, index=True)
     amazon_category_and_sub_category = Column(String)
@@ -30,11 +32,6 @@ class Products(Base):
 #Création de la BDD
 engine = create_engine('sqlite:///products.db', echo=True)
 conn = engine.connect()
-
-# Test
-#stmt = text ( "SELECT * from products limit 5;" )
-#result = conn.execute(stmt)
-#print(result.fetchall())
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
@@ -52,7 +49,16 @@ security = HTTPBasic()
 
 
 def get_product(db: Session, product_uniq_id: int):
-    return db.query(Products).filter(Products.uniq_id == product_uniq_id).first()
+    return db.query(Products).filter(Products.id == product_uniq_id).first()
+
+def get_product_name(db: Session, product_product_name: STRINGTYPE, limit: int =100):
+    return db.query(Products).filter(Products.product_name == product_product_name).limit(limit).all()
+
+def get_product_rating(db: Session, product_average_rating: float, limit: int =100):
+    return db.query(Products).filter(Products.average_rating == product_average_rating).limit(limit).all()
+
+def get_product_price(db: Session, product_price: float, limit: int =100):
+    return db.query(Products).filter(Products.price == product_price).limit(limit).all()
 
 def get_products(db: Session, skip: int = 0, limit: int = 50):
     return db.query(Products).offset(skip).limit(limit).all()
@@ -63,8 +69,29 @@ def search_products(skip: int = 0, limit: int = 100, db: Session = Depends(get_d
     return products
 
 @api.get("/products/{uniq_id}")
-def search_product(uniq_id: int, db: Session = Depends(get_db)):
+def search_product(uniq_id: float, db: Session = Depends(get_db)):
     product = get_product(db, product_uniq_id=uniq_id)
+    if product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return product
+
+@api.get("/products_name/{product_name}")
+def search_product_name(product_name: float, db: Session = Depends(get_db)):
+    product = get_product_name(db, product_product_name=product_name)
+    if product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return product
+
+@api.get("/products_rating/{average_rating}")
+def search_product_rating(average_rating: float, db: Session = Depends(get_db)):
+    product = get_product_rating(db, product_average_rating=average_rating)
+    if product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return product
+
+@api.get("/products_price/{price}")
+def search_product_price(price: float, db: Session = Depends(get_db)):
+    product = get_product_price(db, product_price=price)
     if product is None:
         raise HTTPException(status_code=404, detail="Product not found")
     return product
--- data/database_insertion.py
+++ data/database_insertion.py
@@ -3,6 +3,14 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import logging
+import argparse
+import subprocess
+import os
+import sys
+import pandas as pd
+
+sys.stdout = open('log.txt')
 
 def Load_Data(file_name):
     data = genfromtxt(file_name, delimiter=';', skip_header=1, converters={0: lambda s: str(s)})
@@ -10,8 +18,10 @@ def Load_Data(file_name):
 
 Base = declarative_base()
 
-class Products(Base):
-    __tablename__ = "products"
+#Création de la table product
+class Product(Base):
+    __tablename__ = "product"
+    id = Column(Integer, index=True)
     uniq_id = Column(String, primary_key=True, index=True)
     product_name = Column(String, unique=True, index=True)
     amazon_category_and_sub_category = Column(String)
@@ -24,7 +34,7 @@ class Products(Base):
 
 if __name__ == "__main__":
 
-    #Create the database
+    #Création de la BDD
     engine = create_engine('sqlite:///products.db')
     Base.metadata.create_all(engine)
 
@@ -33,26 +43,30 @@ if __name__ == "__main__":
     session.configure(bind=engine)
     s = session()
 
-    try:
-        file_name = "/files/amazon_co-ecommerce_sample.csv"
-        data = Load_Data(file_name)
+    with open("amazon_co-ecommerce_sample.csv", 'r') as file:
+        data_df = pd.read_csv(file, delimiter=";")
+        data_df.to_sql('product', con=engine, index=True, index_label='id', if_exists='replace')
 
-        for i in data:
-            record = Products(**{
-                'uniq_id' : i[0],
-                'product_name' : i[1],
-                'amazon_category_and_sub_category' : i[2],
-                'manufacturer' : i[3],
-                'price' : i[4],
-                'number_available_in_stock' : i[5],
-                'number_of_reviews' : i[6],
-                'number_of_answered_questions' : i[7],
-                'average_rating' : i[8]
-            })
-            s.add(record)
+    # try:
+    #     file_name = "amazon_co-ecommerce_sample.csv"
+    #     data = Load_Data(file_name)
 
-        s.commit()
-    except:
-        s.rollback()
-    finally:
-        s.close()
+    #     for i in data:
+    #         record = Product(**{
+    #             'uniq_id' : i[0],
+    #             'product_name' : i[1],
+    #             'amazon_category_and_sub_category' : i[2],
+    #             'manufacturer' : i[3],
+    #             'price' : i[4],
+    #             'number_available_in_stock' : i[5],
+    #             'number_of_reviews' : i[6],
+    #             'number_of_answered_questions' : i[7],
+    #             'average_rating' : i[8]
+    #         })
+    #         s.add(record)
+
+    #     s.commit()
+    # except:
+    #     s.rollback()
+    # finally:
+    #     s.close()
